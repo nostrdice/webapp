7d396779d3e97ed8538b68768ac8436f73c08324
Philipp Hoenisch <philipp@coblox.tech>
2024-10-05 20:45:11 +1000

    chore: dprint added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import {DiceNavBar} from "./DiceNavBar.tsx";
+import { DiceNavBar } from "./DiceNavBar.tsx";
 
 function App() {
-    return (
-        <>
-            <DiceNavBar></DiceNavBar>
-            <div className="flex flex-row">
-                <div className="relative h-screen w-1/12 justify-center">
-                </div>
-                <div className="flex w-11/12 flex-col md:flex-row">
-                    <div
-                        className="my-2 size-full rounded-md border-2 border-dashed md:mx-2 md:w-4/12">
-
-                    </div>
-                    <div
-                        className="my-2 size-full rounded-md border-2 border-dashed md:mx-2 md:w-7/12"></div>
-                </div>
-            </div>
-        </>
-    )
-        ;
+  return (
+    <>
+      <DiceNavBar></DiceNavBar>
+      <div className="flex flex-row">
+        <div className="relative h-screen w-1/12 justify-center">
+        </div>
+        <div className="flex w-11/12 flex-col md:flex-row">
+          <div className="my-2 size-full rounded-md border-2 border-dashed md:mx-2 md:w-4/12">
+          </div>
+          <div className="my-2 size-full rounded-md border-2 border-dashed md:mx-2 md:w-7/12"></div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/DiceNavBar.tsx
+++ src/DiceNavBar.tsx
@@ -1,32 +1,43 @@
 export function DiceNavBar() {
-    return (
-        <nav className="border-gray-200 bg-white dark:bg-gray-900">
-            <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
-                <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo"/>
-                    <span
-                        className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">NostrDice</span>
-                </a>
-                <button data-collapse-toggle="navbar-default" type="button"
-                        className="inline-flex size-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
-                        aria-controls="navbar-default" aria-expanded="false">
-                    <span className="sr-only">Open main menu</span>
-                    <svg className="size-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                         viewBox="0 0 17 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                              d="M1 1h15M1 7h15M1 13h15"/>
-                    </svg>
-                </button>
+  return (
+    <nav className="border-gray-200 bg-white dark:bg-gray-900">
+      <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
+        <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+          <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">NostrDice</span>
+        </a>
+        <button
+          data-collapse-toggle="navbar-default"
+          type="button"
+          className="inline-flex size-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
+          aria-controls="navbar-default"
+          aria-expanded="false"
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg className="size-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
+        </button>
 
-                <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-                    <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 md:dark:bg-gray-900 rtl:space-x-reverse">
-                        <li>
-                            <a href="#"
-                               className="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500">Login</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    );
+        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 md:dark:bg-gray-900 rtl:space-x-reverse">
+            <li>
+              <a
+                href="#"
+                className="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+              >
+                Login
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
