15e0b8edc5eb19e73875a2651da873189487c02c
Philipp Hoenisch <philipp@coblox.tech>
2024-10-05 21:00:53 +1000

    chore: some more layouting

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
-import {DiceNavBar} from "./DiceNavBar.tsx";
-import {NostrSocial} from "./NostrSocial.tsx";
-import {NostrGame} from "./NostrGame.tsx";
+import { DiceNavBar } from "./DiceNavBar.tsx";
+import { NostrGame } from "./NostrGame.tsx";
+import { NostrSocial } from "./NostrSocial.tsx";
 
 function App() {
   return (
     <>
-      <DiceNavBar></DiceNavBar>
+      <DiceNavBar />
       <div className="flex flex-row">
-        <div className="relative h-screen w-1/12 justify-center">
-        </div>
         <div className="flex w-11/12 flex-col md:flex-row">
-          <div className="my-2 size-full rounded-md border-2 border-dashed md:mx-2 md:w-4/12">
-              <NostrSocial/>
+          <div className="my-2 size-full rounded-md border-2 border-dashed md:mx-2 md:w-9/12">
+            <NostrGame />
           </div>
-          <div className="my-2 size-full rounded-md border-2 border-dashed md:mx-2 md:w-7/12">
-              <NostrGame/>
+          <div className="my-2 size-full max-h-screen overflow-y-auto rounded-md border-2 border-dashed md:mx-2 md:w-3/12">
+            <NostrSocial />
           </div>
         </div>
       </div>
--- src/NostrSocial.tsx
+++ src/NostrSocial.tsx
@@ -1,3 +1,32 @@
+import { FaBolt } from "react-icons/fa6";
+
+export interface SocialCardProps {
+  none?: boolean;
+}
+
+function SocialCard({}: SocialCardProps) {
+  return (
+    <div className="max-w-sm rounded-lg border border-gray-200 bg-white p-6 shadow dark:border-gray-700 dark:bg-gray-800">
+      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">10x</h5>
+      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+      </p>
+      <button className="inline-flex items-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        Zap to win
+        <FaBolt />
+      </button>
+    </div>
+  );
+}
+
 export function NostrSocial() {
-    return <></>;
+  return (
+    <div className="grid grid-cols-1 gap-4">
+      <SocialCard />
+      <SocialCard />
+      <SocialCard />
+      <SocialCard />
+      <SocialCard />
+    </div>
+  );
 }
